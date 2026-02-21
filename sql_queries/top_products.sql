c7929cf21d8d2eeb20b01e0a363d366bb1321b5a
santiagoacostab17 <santiagoacostab17@gmail.com>
2026-02-20 20:47:53 -0500

    Create top_products.sql

---
+++ sql_queries/top_products.sql
@@ -0,0 +1,9 @@
+-- Top products by revenue
+SELECT 
+    p.product_name,
+    SUM(od.unit_price * od.quantity * (1 - od.discount)) AS revenue
+FROM products p
+JOIN order_details od ON p.product_id = od.product_id
+GROUP BY p.product_name
+ORDER BY revenue DESC
+LIMIT 10;
