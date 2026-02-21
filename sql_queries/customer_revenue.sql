3d8c0dcfeebc74be6fcf20f36ee7d1505c67c8a5
santiagoacostab17 <santiagoacostab17@gmail.com>
2026-02-20 20:56:23 -0500

    Update customer_revenue.sql

--- sql_queries/customer_revenue.sql
+++ sql_queries/customer_revenue.sql
@@ -1,10 +1,10 @@
 -- Top customers by revenue
 SELECT 
-    c.company_name,
+    c.company,
     SUM(od.unit_price * od.quantity * (1 - od.discount)) AS revenue
 FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_details od ON o.order_id = od.order_id
-GROUP BY c.company_name
+JOIN orders o ON c.id = o.customer_id
+JOIN order_details od ON o.id = od.order_id
+GROUP BY c.company
 ORDER BY revenue DESC
 LIMIT 10;
