d52e377d9fd6e6b2a9abd15309a5798e3cc3fe64
santiagoacostab17 <santiagoacostab17@gmail.com>
2026-02-20 20:46:20 -0500

    Create sales_trends.sql

---
+++ sql_queries/sales_trends.sql
@@ -0,0 +1,9 @@
+-- Sales trends over time
+SELECT 
+    YEAR(o.order_date) AS year,
+    MONTH(o.order_date) AS month,
+    SUM(od.unit_price * od.quantity * (1 - od.discount)) AS revenue
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+GROUP BY year, month
+ORDER BY year, month;
