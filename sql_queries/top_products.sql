7dfb51304c4c28f46d7e2c7fe2f1d54f76587e06
santiagoacostab17 <santiagoacostab17@gmail.com>
2026-02-20 20:59:54 -0500

    Update top_products.sql

--- sql_queries/top_products.sql
+++ sql_queries/top_products.sql
@@ -3,7 +3,7 @@ SELECT
     p.product_name,
     SUM(od.unit_price * od.quantity * (1 - od.discount)) AS revenue
 FROM products p
-JOIN order_details od ON p.product_id = od.product_id
+JOIN order_details od ON p.id = od.product_id
 GROUP BY p.product_name
 ORDER BY revenue DESC
 LIMIT 10;
