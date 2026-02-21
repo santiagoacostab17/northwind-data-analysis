1f54956788eceef2c1940b5bc22727f3c9c1d58f
santiagoacostab17 <santiagoacostab17@gmail.com>
2026-02-20 20:54:34 -0500

    Update sales_by_country.sql

--- sql_queries/sales_by_country.sql
+++ sql_queries/sales_by_country.sql
@@ -1,9 +1,9 @@
 -- Revenue by country
 SELECT 
-    c.country,
+    c.country_region,
     SUM(od.unit_price * od.quantity * (1 - od.discount)) AS revenue
 FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_details od ON o.order_id = od.order_id
-GROUP BY c.country
+JOIN orders o ON c.id = o.customer_id
+JOIN order_details od ON o.id = od.order_id
+GROUP BY c.country_region
 ORDER BY revenue DESC;
